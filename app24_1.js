d87f32149689d6a8090be14c972b44402503da33
Pedro Antonio Sanchez Romero <98395353+pedroasr@users.noreply.github.com>
2022-06-07 18:49:50 +0200

    Add files via upload

--- app24_1.js
+++ app24_1.js
@@ -1,93 +1,69 @@
-require('dotenv').config();
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const fs = require('fs');
-const pcap = require('pcap');
-const colors = require('colors')
+require("dotenv").config();
+const pcap = require("pcap");
+const colors = require("colors");
 
+const Database = require("better-sqlite3");
+const db = new Database("Sniffer-Wifi.db");
+const createTable =
+  "CREATE TABLE IF NOT EXISTS ProbeRequestFrames ('timestamp', 'snifferId', 'SSID', 'RSSI', 'MAC_origen', 'canal')";
+db.exec(createTable);
+const insertInto = db.prepare(
+  "INSERT INTO ProbeRequestFrames (timestamp, snifferId, SSID, RSSI, MAC_origen, canal) VALUES (?, ?, ?, ?, ?, ?)"
+);
 
 // ====================== SNIFFER WIFI ==================================
 
-const snifferId = 'sniffer2.4Ghz_1'; //cambiar por id del sniffer en el que se ejecute
-let files = fs.readdirSync('./results');
-const {
-    parseSSID,
-    parseType,
-    parseFreq
-} = require('./functions');
+const snifferId = "sniffer2.4Ghz_1"; //cambiar por id del sniffer en el que se ejecute
+const { parseSSID, parseType, parseFreq } = require("./functions");
 
 var timestamp = new Date();
 
-var csvWriter = createCsvWriter({
-    path: `./results/Sniffer_2.4_${files.length}_${timestamp.getFullYear()}_${timestamp.getMonth()+1}_${timestamp.getDate()}.csv`,
-    fieldDelimiter: ";",
-    header: [
-        { id: 'timestamp', title: 'TIMESTAMP' },
-        { id: 'snifferId', title: 'SNIFFERID' },
-        { id: 'SSID', title: 'SSID' },
-        { id: 'RSSI', title: 'RSSI' },
-        { id: 'MAC_origen', title: 'MAC_O' },
-        { id: 'canal', title: 'CANAL' }
-    ]
-});
-
-
 function init() {
+  console.log("iniciando capturas...");
+  // creamos sesion de pcap indicando interfaz (en modo monitor con airmon-ng) y filtros
+  // sustituir interfaz por la del dispositivo en el que se ejecuta la app
+  var pcapSession = pcap.createSession("wlan1", {
+    filter: "type mgt subtype probe-req",
+  });
 
-    console.log("iniciando capturas...");
-    // creamos sesion de pcap indicando interfaz (en modo monitor con airmon-ng) y filtros
-    // sustituir interfaz por la del dispositivo en el que se ejecuta la app
-    var pcapSession = pcap.createSession('wlan1', { filter: 'type mgt subtype probe-req' });
+  let SSID;
+  let RSSI;
+  let MAC_origen;
+  let frec;
+  let canal;
+  let date;
 
-    let SSID;
-    let RSSI;
-    let MAC_origen;
-    let frec;
-    let canal;
-    let date;
+  pcapSession.on("packet", (rawPacket) => {
+    var length_RT = rawPacket.buf[2];
+    frec = parseFreq(rawPacket.buf, length_RT);
+    canal = (frec % 2407) / 5;
 
-    pcapSession.on('packet', (rawPacket) => {
-        var length_RT = rawPacket.buf[2];
-        frec = parseFreq(rawPacket.buf, length_RT);
-        canal = (frec % 2407) / 5;
-        
-        if (canal == 1){
-            var packet = pcap.decode.packet(rawPacket);
-            var tipo = parseType(rawPacket.buf, length_RT);
-            SSID = parseSSID(rawPacket.buf, length_RT, tipo);
-            RSSI = packet.payload.fields.antenna_signal;
-            MAC_origen = packet.payload.ieee802_11Frame.shost.toString(16);
-            date = `${timestamp.getFullYear()}-${timestamp.getMonth()+1}-${timestamp.getDate()} ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}:${timestamp.getMilliseconds()}`;
+    if (canal == 1) {
+      var packet = pcap.decode.packet(rawPacket);
+      var tipo = parseType(rawPacket.buf, length_RT);
+      SSID = parseSSID(rawPacket.buf, length_RT, tipo);
+      RSSI = packet.payload.fields.antenna_signal;
+      MAC_origen = packet.payload.ieee802_11Frame.shost.toString(16);
+      date = `${timestamp.getFullYear()}-${
+        timestamp.getMonth() + 1
+      }-${timestamp.getDate()} ${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}:${timestamp.getMilliseconds()}`;
 
-            var disp = `===================\n`.green +
-                `===PROBE RESQUEST===\n`.green +
-                `===================\n`.green +
-                `ssid: ${ SSID }\n` +
-                `RSSI: ${ RSSI } dBm\n` +
-                `TimeStamp: ${date}\n` +
-                `MAC origen: ${ MAC_origen }\n` +
-                `Canal: ${ canal }\n`
+      var disp =
+        `====================\n`.green +
+        `===PROBE RESQUEST===\n`.green +
+        `====================\n`.green +
+        `ssid: ${SSID}\n` +
+        `RSSI: ${RSSI} dBm\n` +
+        `TimeStamp: ${date}\n` +
+        `MAC origen: ${MAC_origen}\n` +
+        `Canal: ${canal}\n`;
 
-            console.log(disp)
+      console.log(disp);
 
-            var datos_prob = {
-                snifferId,
-                timestamp: `${ date }`,
-                SSID,
-                RSSI: `${ RSSI }`,
-                MAC_origen,
-                canal
-            }
-
-
-            csvWriter.writeRecords([datos_prob]) // returns a promise
-                .then(() => {
-                    console.log('Guardado en csv local'.blue);
-                }); 
-          }
-
-    });
+      insertInto.run(date, snifferId, SSID, RSSI, MAC_origen, canal);
+    }
+  });
 }
 
 console.log("esperando 5 seg a que se inicie script");
 setTimeout(init, 5000);
-
