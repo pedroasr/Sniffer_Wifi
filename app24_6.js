28b5cfd31f105246ae4a3e61e90ee8f50305fd6a
Daniel <griefros@gmail.com>
2022-06-14 18:19:11 +0200

    Todos modificados

--- app24_6.js
+++ app24_6.js
@@ -11,6 +11,15 @@ const insertInto = db.prepare(
   "INSERT INTO ProbeRequestFrames (timestamp, snifferId, SSID, RSSI, MAC_origen, canal) VALUES (?, ?, ?, ?, ?, ?)"
 );
 
+/*=========================== MQTT =========================*/
+
+const connectUrl = 'mqtt://192.168.200.106'
+const client = mqtt.connect(connectUrl)
+
+client.on('connect', function () {
+        console.log("Connected to MQTT URL")
+})
+
 // ====================== SNIFFER WIFI ==================================
 
 const snifferId = "sniffer2.4Ghz_6"; //cambiar por id del sniffer en el que se ejecute
@@ -59,6 +68,14 @@ function init() {
         `Canal: ${canal}\n`;
 
       console.log(disp);
+
+      wifidata.ssid = SSID;
+      wifidata.rssi = RSSI;
+      wifidata.timestamp = date;
+      wifidata.OrigMAC = MAC_origen;
+      wifidata.canal = canal;
+      
+      client.publish("CRAIUPCT_WifiData",JSON.stringify(wifidata));
 
       insertInto.run(date, snifferId, SSID, RSSI, MAC_origen, canal);
     }
