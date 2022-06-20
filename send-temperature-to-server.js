f501bcda51433cb8a78a5fa03ee31559c74230c3
Pedro Antonio <pedroantoniosr96@gmail.com>
2022-06-20 09:44:03 +0200

    Script for Digio Teamcamp

---
+++ send-temperature-to-server.js
@@ -0,0 +1,20 @@
+exec = require("child_process").exec;
+
+setInterval(function () {
+  exec(
+    "cat /sys/class/thermal/thermal_zone0/temp",
+    function (error, stdout, stderr) {
+      if (error !== null) {
+        console.log("exec error: " + error);
+      } else {
+        var date = new Date();
+        var temp = parseFloat(stdout / 1000);
+        var dataToSend = {
+          date: date,
+          temperature: temp,
+        };
+        console.log("temperatureUpdate", dataToSend);
+      }
+    }
+  );
+}, 5000);
