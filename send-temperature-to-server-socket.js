eff1a331b6815574205b08bf30686b9c495df6ac
Pedro Antonio <pedroantoniosr96@gmail.com>
2022-06-29 09:47:32 +0200

    Update WebSocket

--- send-temperature-to-server-socket.js
+++ send-temperature-to-server-socket.js
@@ -1,26 +1,24 @@
 import { io } from "socket.io-client";
 import exec from "child_process";
 
-const socket = io.connect("*****");
+const socket = io.connect("http://api.digiot.teamcamp.ovh:3030/data");
 
-socket.on("connection", function (socket) {
-  setInterval(function () {
-    exec.exec(
-      "cat /sys/class/thermal/thermal_zone0/temp",
-      function (error, stdout, stderr) {
-        if (error !== null) {
-          console.log("exec error: " + error);
-        } else {
-          const date = new Date().getTime();
-          const temp = parseFloat(stdout / 1000);
-          const dataToSend = {
-            deviceID: "PachiBerry",
-            value: temp,
-            timestamp: date,
-          };
-          socket.emit("temperatureUpdate", dataToSend);
-        }
+setInterval(function () {
+  exec.exec(
+    "cat /sys/class/thermal/thermal_zone0/temp",
+    function (error, stdout, stderr) {
+      if (error !== null) {
+        console.log("exec error: " + error);
+      } else {
+        const date = new Date().getTime();
+        const temp = parseFloat(stdout / 1000);
+        const dataToSend = {
+          sensorId: "PachiBerry",
+          value: temp,
+          timestamp: date,
+        };
+        socket.emit("temperatureUpdate", dataToSend);
       }
-    );
-  }, 1000);
-});
+    }
+  );
+}, 2000);
