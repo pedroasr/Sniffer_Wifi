0e5cc28adc82a9ffd99db52279902304208b4e9e
Daniel <griefros@gmail.com>
2022-10-05 15:24:42 +0200

    retaking keepalive

--- app24_1.js
+++ app24_1.js
@@ -184,28 +184,27 @@ cron.schedule("0 */5 * * * *", () => {
   setTimeout(init, 5000);
 });
 
-//cron.schedule("*/30 * * * * *", () => {//Keep-alive
-/*
+let ka = wifidata
+
+setInterval(()=>{
+
   exec(
     "cat /sys/class/thermal/thermal_zone0/temp",
     function (error, stdout, stderr) {
       if (error !== null) {
         console.log("exec error: " + error);
       } else {
-        wifidata.rssi = parseFloat(stdout / 1000);
-        wifidata.canal = 0
-        wifidata.timestamp = getFullDate()
-        wifidata.vendorspecific = "KeepAlive"
+        ka.rssi = parseFloat(stdout / 1000);
+        ka.canal = 0
+        ka.timestamp = getFullDate()
+        ka.ssid = "KeepAlive"
       
 
-        
-        client.publish("CRAIUPCT_WifiData", JSON.stringify(wifidata));
+        client.publish("CRAIUPCT_WifiData", JSON.stringify(ka));
         
 
       }
     }
   );
 
-
-
-});*/
+}, 30000);
